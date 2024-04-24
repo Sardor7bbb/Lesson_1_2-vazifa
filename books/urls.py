dbb0963a1670fce5ee567eea5b6a035706e099a0
Pac_Man <pac_man@gmail.com>
2024-04-24 17:58:36 +0500

    Create a project

---
+++ books/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+
+from books.views import home_view
+
+urlpatterns = [
+    path('list/', home_view),
+
+]
--- conf/settings.py
+++ conf/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'conf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templets'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
